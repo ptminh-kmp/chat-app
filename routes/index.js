ba94589057ca38499af75c0c660db71ed0ef83e6
ptminh-kmp <ptminh.gun@gmail.com>
2019-07-18 22:47:26 +0700

    add static pages

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,13 @@
 import express from 'express';
 let router = express.Router();
 
-import contactModel from '../models/contact.model';
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  return res.render('main/master');
 });
 
-router.get('/test-database', async (req, res) => {
-  try {
-    let item = {
-      userId: '1321321',
-      contactId: 'dawdadwad',
-    };
-
-    let contact = await contactModel.createNew(item);
-
-    res.send(contact);
-
-  } catch (error) {
-    console.log(error);
-  }
+router.get('/login-register', (req, res) => {
+  return res.render('auth/loginRegister');
 });
 
 module.exports = router;
