1fb5b48fd64f0ab4926349a020de7c328ad1c80c
ptminh-kmp <ptminh.gun@gmail.com>
2019-07-22 21:08:50 +0700

    register user part 1

---
+++ lang/vi.js
@@ -0,0 +1,7 @@
+export const transValidation = {
+    email_incorrect: 'Email phải có dạng example@gmail.com!',
+    gender_incorrect: 'Trường giới tính bị sai?',
+    password_incorrect: 'Mật khẩu chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.',
+    password_confirm_incorrect: 'Nhập lại mật khẩu chưa chính xác.'
+
+};
