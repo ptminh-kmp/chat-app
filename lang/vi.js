780f4c742b199cb6413728782780c0233f705b82
ptminh-kmp <ptminh.gun@gmail.com>
2019-08-04 20:51:46 +0700

    send mail confirm account

--- lang/vi.js
+++ lang/vi.js
@@ -9,10 +9,25 @@ export const transErrors = {
     account_in_use: 'Email này đã được sử dụng.',
     account_removed: 'Tài khoản này đã bị gỡ khỏi hệ thống, nếu tin rằng điều này là hiểu lầm, vui lòng liên hệ với bộ phận hỗ trợ.',
     account_not_active: 'Email chưa được kích hoạt tài khoản.',
+    token_undefined: 'Token không tồn lại!'
 };
 
 export const transSuccess = {
     userCreated: (userEmail) => {
         return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email của bạn để kích hoạt tài khoản trước khi đăng nhập.`;
-    }
+    },
+    account_actived: 'Tài khoản đã kích hoạt thành công, bạn đã có thể đăng nhập vào ứng dụng'
+};
+
+export const transMail = {
+    subject: 'Chat: Xác nhận tài khoản.',
+    template: (linkVerify) => {
+        return `
+            <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên ứng dụng Chat.</h2>
+            <h3>Vui lòng click vào liên kết bên dưới để xác nhận kích hoạt tài khoản.</h3>
+            <h3><a href="${linkVerify}" target="blank">${linkVerify}</a></h3>
+            <h4>Xin cám ơn,</h4>
+        `;
+    },
+    send_failed: 'Có lỗi trong quá trình gửi email, vui lòng liên hệ lại với bộ phận hỗ trợ.'
 };
