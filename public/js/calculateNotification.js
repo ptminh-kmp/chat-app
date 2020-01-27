36ded348813240493d19a3c8cc111b578e8a9c78
Minh PT <ptminh.gun@gmail.com>
2020-01-27 21:16:39 +0700

    add contact realtime

---
+++ public/js/calculateNotification.js
@@ -0,0 +1,25 @@
+function decreaseNumberNotification(className) {
+    let currentValue = +$(`.${className}`).text();
+    currentValue -= 1;
+
+    if (currentValue === 0) {
+        $(`.${className}`).css('display', 'none').html('');
+    } else {
+        $(`.${className}`).css('display', 'block').html(currentValue);
+    }
+};
+
+function increaseNumberNotification(className) {
+    let currentValue = +$(`.${className}`).text();
+
+    if (isNaN(currentValue))
+        currentValue = 0;
+
+    currentValue += 1;
+
+    if (currentValue === 0 || isNaN(currentValue)) {
+        $(`.${className}`).css('display', 'none').html('');
+    } else {
+        $(`.${className}`).css('display', 'block').html(currentValue);
+    }
+};
